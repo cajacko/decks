35b16e8a9802ae4a87789465a54b6fac87dffdf3
Charlie Jackson <charlie@charliejackson.com>
2025-03-12 17:53:40 +0000

    improvement(urls): ✨ improve built in example ids for urls
    branch dev

--- constants/exampleDecks/index.ts
+++ constants/exampleDecks/index.ts
@@ -3,9 +3,10 @@ import jugglingPractice from "./jugglingPractice";
 import morningPages from "./morningPages";
 import playingCards from "./playingCards";
 
+// These are the ID's used in the nav, so make sure they are url friendly
 export default {
-  playingCards,
+  "playing-cards": playingCards,
   charades,
-  morningPages,
-  jugglingPractice,
+  "juggling-practice": jugglingPractice,
+  "morning-pages": morningPages,
 };
